39a714e4e3b6683358e62e8fc0bfc6dee56c9adb
ctrlbold <clemaire@gmail.com>
2017-05-19 15:35:38 +0200

    wtf

--- tests/appveyor.sqlserver.ps1
+++ tests/appveyor.sqlserver.ps1
@@ -1,6 +1,6 @@
 # Imports some assemblies
 Write-Output "Importing dbatools"
-Import-Module dbatools
+Import-Module C:\projects\dbatools\dbatools.psd1
 
 # This script spins up two local instances
 $sql2008 = "localhost\sql2008r2sp2"
