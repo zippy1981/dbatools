1863bf65dc4b5da4383204f17cbc630c9740b152
Justin Dearing <zippy1981@gmail.com>
2018-04-25 14:47:05 -0400

    lets fix mstest

--- tests/appveyor.post.ps1
+++ tests/appveyor.post.ps1
@@ -6,15 +6,25 @@ codecov -f PesterResultsCoverage.json --flag "ps,$($env:SCENARIO.toLower())" | O
 # DLL unittests only in default scenario
 if($env:SCENARIO -eq 'default') {
   Write-Host -Object "appveyor.post: DLL unittests"  -ForeGroundColor DarkGreen
+  $msTestArgs = [string]::Join('', @(
+    '/logger:Appveyor',
+    'bin\projects\dbatools\dbatools.Tests\bin\Debug\dbatools.Tests.dll',
+    '/TestAdapterPath:bin\projects\dbatools\packages\MSTest.TestAdapter.1.2.1\build\_common',
+    '/Enablecodecoverage'
+  ))
+  
   OpenCover.Console.exe `
     -register:user `
     -target:"vstest.console.exe" `
-    -targetargs:"/logger:Appveyor bin\projects\dbatools\dbatools.Tests\bin\Debug\dbatools.Tests.dll" `
+    -targetargs: $msTestArgs `
     -output:"coverage.xml" `
     -filter:"+[dbatools]*" `
     -returntargetcode
   Push-AppveyorArtifact coverage.xml -FileName "OpenCover C# Report"
   codecov -f "coverage.xml" --flag "dll,$($env:SCENARIO.toLower())" | Out-Null
+
+  Write-Host -Object "Listing Loggers" -ForeGroundColor Magenta
+  vstest.console.exe /ListLoggers
 }
 $sw.Stop()
 Update-AppveyorTest -Name "appveyor.post" -Framework NUnit -FileName "appveyor.post.ps1" -Outcome Passed -Duration $sw.ElapsedMilliseconds
